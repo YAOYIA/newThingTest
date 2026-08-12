ce69370a56c43a78d104f64ef75a70f66819eaca
wangkaiyi <kaiyiwang7@163.com>
2026-08-12 22:23:41 +0800

    update

---
+++ learnSTL/test/test_vector.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "../vector/vector.h"
+
+using wstl::vector;
+
+int main()
+{
+    vector<int> vec;
+    std::cout << "初始大小：" << vec.size() << std::endl;
+    
+    for (size_t i = 0; i < 5; i++)
+    {
+        vec.push_back(i*10);
+        std::cout << "插入数据：" << i*10 << "后。大小：" << vec.size() << ", 容量: " << vec.capacity() << std::endl;
+    }
+    
+    //读取
+    std::cout <<"\n当前内容:";
+    for (size_t i = 0; i < 5; i++)
+    {
+        std::cout << vec[i] << " ";
+    }
+    std::cout<<std::endl;
+
+    // vec[0] = 999;
+    // std::cout << "修改第一个元素后: " << vec[0] << std::endl;
+
+    // 测试 pop_back
+    vec.pop_back();
+    std::cout << "\n执行一次 pop_back 后 - 大小: " << vec.size() << ", 容量: " << vec.capacity() << std::endl;
+
+    return 0; // 程序结束时，vec 的析构函数会自动调用 delete[] 释放堆内存！
+    
+}
---
+++ learnSTL/vector/vector.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <iostream>
+
+namespace wstl
+{
+template<typename T>
+class vector
+{
+public:
+    //构造函数：初始化为空状态
+    vector()
+    {
+        data = nullptr;
+        _size = 0;
+        _capacity = 0;
+    }
+    // vector(int capacity):_capacity(capacity)
+    // {
+    //     data = nullptr;
+    //     _size = 0;
+    // }
+    ~vector()
+    {
+        delete [] data;
+    }
+
+    size_t size() {return _size;}
+    size_t capacity() {return _capacity;}
+
+    void push_back(const T& value)
+    {
+        if(_size == _capacity)
+        {
+            size_t newCapacity = (_capacity == 0)? 1 : _capacity*2;
+            //开辟新空间
+            T* newData = new T[newCapacity];
+
+            //搬移旧数据
+            if (data != nullptr)
+            {
+                for (size_t i = 0; i < _size; i++)
+                {
+                    newData[i] = data[i];
+                }
+                delete [] data;
+            }
+            //更新指针和容量
+            data = newData;
+            _capacity = newCapacity;
+        }
+        data[_size] = value;
+        _size++;
+    }
+
+    void pop_back()
+    {
+        //不需要清理那块内存，下次再写会覆盖
+        if(_size > 0)
+        {
+            _size --;
+        }
+    }
+    const T& operator[](size_t index) const
+    {
+        return data[index];
+    }
+    // T& operator[](size_t index)
+    // {
+    //     return data[index];
+    // }
+private:
+    T *data;
+    size_t _size;
+    size_t _capacity;
+};//vector
+
+}//wstl
